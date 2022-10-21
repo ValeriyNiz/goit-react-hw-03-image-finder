67704ed4f834a0dc2985faca766617eb8de58616
Valeriy Nizovtsev <nizucer123@gmail.com>
2022-10-21 20:10:21 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
+// import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 import PropTypes from 'prop-types';
 
 const ImageGallery = props => {
-  return (
-    <ul>
-      <ImageGalleryItem />
-    </ul>
-  );
+  return <ul>{/* <ImageGalleryItem /> */}</ul>;
 };
 
 ImageGallery.propTypes = {};
